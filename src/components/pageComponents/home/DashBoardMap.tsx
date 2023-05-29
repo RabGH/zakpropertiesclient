f0542aa0afbef480b69a3412fcace0e9f4cec77d
RabGh <rabiighais@gmail.com>
2023-05-29 22:34:27 +0300

    babel, map fix, and update next

--- src/components/pageComponents/home/DashBoardMap.tsx
+++ src/components/pageComponents/home/DashBoardMap.tsx
@@ -1,7 +1,7 @@
-import React, { Ref, useEffect, useRef, useState } from "react";
+import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { Project, Property } from "@lib/types";
-import L, { LatLngBounds, LatLngExpression } from "leaflet";
+import L from "leaflet";
 
 interface DashBoardMapProps {
   properties: Property[];
@@ -12,27 +12,8 @@ const DashBoardMap: React.FC<DashBoardMapProps> = ({
   properties,
   projects,
 }) => {
-  const mapRef = useRef<React.RefObject<typeof MapContainer>>(null);
-
-  useEffect(() => {
-    if (mapRef.current && (properties.length > 0 || projects.length > 0)) {
-      const bounds = new LatLngBounds(
-        [51.505, -0.09] as LatLngExpression, // southWest
-        [51.5, -0.1] as LatLngExpression // northEast
-      );
-      [...properties, ...projects].forEach((item) => {
-        bounds.extend([
-          item.location.lat as number,
-          item.location.lng as number,
-        ] as LatLngExpression);
-      });
-      const map = mapRef.current;
-      map.fitBounds(bounds);
-    }
-  }, [properties, projects]);
-
-  const center: LatLngExpression = L.latLng(25.276987, 55.296249);
-  // const [map, setMap] = useState(null);
+  const center: L.LatLngExpression = [25.2697, 55.3095];
+  const zoom = 10;
 
   return (
     <div>
@@ -40,8 +21,7 @@ const DashBoardMap: React.FC<DashBoardMapProps> = ({
         scrollWheelZoom={false}
         style={{ height: "400px", width: "100%" }}
         center={center}
-        zoom={10}
-        ref={mapRef as React.MutableRefObject<typeof MapContainer | null>}
+        zoom={zoom}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
