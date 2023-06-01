a446f6ba08a2eec73b6784fa795ae47b9d0fd0e6
RabGh <rabiighais@gmail.com>
2023-06-01 07:09:25 +0300

    Fix preview sanity next api files

--- src/pages/api/exit-preview.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { clearPreviewData } from "next/client";
-
-export default async function exit(req: NextApiRequest, res: NextApiResponse) {
-  clearPreviewData(req, res);
-
-  res.writeHead(307, { Location: "/" });
-  res.end();
-}
