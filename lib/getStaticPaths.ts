9ef89adfce43ac7c6d75bb302fd7186a18242f9d
RabGh <rabiighais@gmail.com>
2023-08-02 04:48:17 +0300

    get static paths query all developers

--- lib/getStaticPaths.ts
+++ lib/getStaticPaths.ts
@@ -1,5 +1,5 @@
 import { sanityClient } from "./sanity";
-import { Property, Project } from "./types";
+import { Property, Project, Developer } from "./types";
 
 export async function getAllPropertySlugs() {
   const query = `*[_type == "property"]{slug}`;
@@ -10,5 +10,11 @@ export async function getAllPropertySlugs() {
 export async function getAllProjectSlugs() {
   const query = `*[_type == "projects"]{slug}`;
   const slugs = await sanityClient.fetch<Project[]>(query);
+  return slugs.map((slug) => ({ params: { slug: slug.slug.current } }));
+}
+
+export async function getAllDeveloperSlugs() {
+  const query = `*[_type == "developer"]{slug}`;
+  const slugs = await sanityClient.fetch<Developer[]>(query);
   return slugs.map((slug) => ({ params: { slug: slug.slug.current } }));
 }
