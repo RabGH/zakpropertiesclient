7bed7e9c832356cf686e58ada527b8c2dc8e536d
RabGh <rabiighais@gmail.com>
2023-08-11 02:26:12 +0300

    Add development to static paths

--- lib/getStaticPaths.ts
+++ lib/getStaticPaths.ts
@@ -1,9 +1,15 @@
 import { sanityClient } from "./sanity";
 import { Property, Project, Developer } from "./types";
 
-export async function getAllPropertySlugs() {
-  const query = `*[_type == "property"]{slug}`;
-  const slugs = await sanityClient.fetch<Property[]>(query);
+export async function getAllDeveloperSlugs() {
+  const query = `*[_type == "developer"]{slug}`;
+  const slugs = await sanityClient.fetch<Developer[]>(query);
+  return slugs.map((slug) => ({ params: { slug: slug.slug.current } }));
+}
+
+export async function getAllDevelopmentSlugs() {
+  const query = `*[_type == "development"]{slug}`;
+  const slugs = await sanityClient.fetch<Developer[]>(query);
   return slugs.map((slug) => ({ params: { slug: slug.slug.current } }));
 }
 
@@ -13,8 +19,8 @@ export async function getAllProjectSlugs() {
   return slugs.map((slug) => ({ params: { slug: slug.slug.current } }));
 }
 
-export async function getAllDeveloperSlugs() {
-  const query = `*[_type == "developer"]{slug}`;
-  const slugs = await sanityClient.fetch<Developer[]>(query);
+export async function getAllPropertySlugs() {
+  const query = `*[_type == "property"]{slug}`;
+  const slugs = await sanityClient.fetch<Property[]>(query);
   return slugs.map((slug) => ({ params: { slug: slug.slug.current } }));
 }
