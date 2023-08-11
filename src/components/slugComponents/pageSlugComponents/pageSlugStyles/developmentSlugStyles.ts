b680eae58fd455028f8b94389817476b5ad51be9
RabGh <rabiighais@gmail.com>
2023-08-11 19:20:39 +0300

    Add development page to pages

---
+++ src/components/slugComponents/pageSlugComponents/pageSlugStyles/developmentSlugStyles.ts
@@ -0,0 +1,25 @@
+import { useTheme } from "@mui/material";
+
+export const getDevelopmentPageStyles = () => {
+  const muiTheme = useTheme();
+
+  return {
+    buttonStyles: {
+      "&:hover": {
+        backgroundColor: muiTheme.palette.primary.light,
+      },
+      mb: "3rem",
+      mt: "3rem",
+    },
+
+    dividerStyles: {
+      width: "90%",
+      mt: "0.5rem",
+      mb: "0.5rem",
+      "@media (max-width: 481px)": {
+        mt: "1rem",
+        mb: "1rem",
+      },
+    },
+  };
+};
---
+++ src/pages/developments/[slug].tsx
@@ -0,0 +1,129 @@
+import { sanityClient } from "@lib/sanity";
+import { getAllDeveloperSlugs } from "@lib/getStaticPaths";
+import {
+  formatPrice,
+  formatArea,
+  formatNumberWithCommas,
+  isMultiple,
+} from "@lib/utils";
+import Link from "next/link";
+import { Box, Divider, Typography, Button } from "@mui/material";
+import { Project, Developer, Development, PageContext } from "@lib/types";
+import DevelopmentAmenities from "@/components/slugComponents/pageSlugComponents/amenities/DevelopmentAmenities";
+import ImageCarousel from "@/components/slugComponents/pageSlugComponents/imageSlugComponents/ImageGallerySlick";
+import ViewAllPhotos from "@/components/slugComponents/pageSlugComponents/imageSlugComponents/viewAllPhotos";
+import {
+  mainContainer,
+  mainImageContainer,
+  viewPhotosBox,
+} from "@/components/slugComponents/pageSlugComponents/imageSlugComponents/imageCarouselStyles";
+import { getDevelopmentPageStyles } from "@/components/slugComponents/pageSlugComponents/pageSlugStyles/developmentSlugStyles";
+import dynamic from "next/dynamic";
+
+interface ProjectList {
+  projects: Project[];
+}
+
+type DevelopmentProps = Development & ProjectList;
+
+const Developments = ({
+  _id,
+  title,
+  description,
+  createdAt,
+  location,
+  developmentPropertyTypes,
+  mainDevelopmentImage,
+  developmentImages,
+  developmentsDeveloper,
+  developmentProjects,
+  developmentAveragePrice,
+  addresses,
+  developmentBuiltUpArea,
+  bulletPoints,
+  developmentAmenities,
+  presentation,
+}: DevelopmentProps) => {
+  const styles = getDevelopmentPageStyles();
+
+  return (
+    <>
+      <Box sx={mainContainer}>
+        <Box sx={mainImageContainer}>
+          <ImageCarousel
+            mainImage={mainDevelopmentImage}
+            images={developmentImages}
+            alt={title}
+          />
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export async function getStaticPaths() {
+  const paths = await getAllDeveloperSlugs();
+
+  return { paths, fallback: false };
+}
+
+export async function getStaticProps(context: PageContext) {
+  const slug = context.params.slug;
+  const query = `*[ _type == "development" && slug.current == $slug][0]{
+    _id,
+    title,
+    description,
+    createdAt,
+    location->{
+        lat,
+        lng,
+    },
+    developmentPropertyTypes[],
+    mainDevelopmentImage,
+    developmentImages[],
+    developmentDeveloper,
+    developmentProjects,
+    developmentAveragePrice[],
+    addresses[],
+    developmentBuildUpArea[],
+    bulletPoints[],
+    developmentAmenities->{
+        name,
+        developmentsAmenities[],
+    },
+    presentation,
+  }`;
+
+  const development = await sanityClient.fetch(query, { slug });
+
+  if (!development) {
+    return {
+      props: null,
+      notFound: true,
+    };
+  } else {
+    return {
+      props: {
+        _id: development._id,
+        title: development.title,
+        description: development.description,
+        createdAt: development.createdAt ?? null,
+        location: development.location,
+        developmentPropertyTypes: development.developmentPropertyTypes ?? [],
+        mainDevelopmentImage: development.mainDevelopmentImage ?? null,
+        developmentImages: development.developmentImages ?? [],
+        developmentDeveloper: development.developmentDeveloper ?? null,
+        developmentProjects: development.developmentProjects ?? [],
+        developmentAveragePrice: development.developmentAveragePrice ?? [],
+        addresses: development.addresses ?? [],
+        developmentBuildUpArea: development.developmentBuildUpArea ?? [],
+        bulletPoints: development.bulletPoints ?? [],
+        developmentAmenities: development.developmentAmenities ?? [],
+        presentation: development.presentation ?? null,
+      },
+      revalidate: 60,
+    };
+  }
+}
+
+export default Developments;
