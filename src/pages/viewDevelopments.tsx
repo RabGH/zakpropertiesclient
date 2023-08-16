c1331dca288745d2becf9d869f6466e0a9a702ab
RabGh <rabiighais@gmail.com>
2023-08-16 19:50:43 +0300

    Duplicate temp base template for development page

--- src/pages/viewDevelopments.tsx
+++ src/pages/viewDevelopments.tsx
@@ -1,44 +1,93 @@
-import { Container, Box, Typography, Button } from "@mui/material";
-import { useTheme } from "@mui/material";
+import React from "react";
+import { Typography, Box, Grid, Button } from "@mui/material";
+import Divider from "@mui/material/Divider";
+import { Development } from "@lib/types";
+import { GetStaticProps } from "next";
+import { sanityClient } from "@lib/sanity";
+import { previewClient } from "@lib/client";
+import Link from "next/link";
 import Head from "next/head";
+import { getAllDevelopmentStyles } from "@/components/pageComponents/developments/allDevelopmentStyles";
+// import ProjectAllCards from "@/components/slugComponents/cardSlugs/projectCards/ProjectAllCards";
 
-const Styles = {
-  root: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-    height: "100vh",
-  },
-  title: {
-    marginBottom: "1rem",
-  },
-  buttons: {
-    display: "flex",
-    gap: "1rem",
-  },
-};
+interface AllDevelopmentProps {
+  developments?: Development[];
+}
 
-function Error() {
+function AllDevelopments({ developments }: AllDevelopmentProps) {
+  const styles = getAllDevelopmentStyles();
+
   return (
-    <Container sx={Styles.root}>
+    <Box sx={styles.mainContainer}>
       <Head>
-        <title>ZakProperties Error Page</title>
-        <meta name="description" content="ZakProperties Error Page" />
+        <title>ZakProperties Development Page</title>
+        <meta name="description" content="ZakProperties Development Page" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Typography variant="h4" sx={Styles.title}>
-        Error
+      <Typography variant="h3" component="h1" sx={styles.titleStyles}>
+        Featured Developments
       </Typography>
-      <Typography variant="body1">Error, return to home page.</Typography>
-      <Box sx={Styles.buttons}>
-        <Button onClick={() => (window.location.href = "/")}>Home</Button>
-        <Button onClick={() => (window.location.href = "/contactUs")}>
-          Contact Us
+      <Typography variant="body1" sx={styles.introText}>
+        Welcome to our developments page, where you can find the latest and most
+        luxurious real estate projects in Dubai UAE. Whether you are looking for
+        apartments, villas, townhouses, or off-plan properties, we have
+        something for everyone. Browse through our featured developments and
+        discover their unique ready and offplan projects. You can also contact
+        us for more information and inquiries about any of the developments.
+      </Typography>
+
+      {developments && (
+        <Box sx={styles.main}>
+          <Box sx={styles.mainBox}>
+            <Box>
+              <Grid container spacing={2} sx={{ mt: "2rem" }}>
+                {developments?.map((developments) => (
+                  <Grid
+                    item
+                    key={developments._id}
+                    xs={12}
+                    sm={6}
+                    md={4}
+                    lg={3}
+                  >
+                    {/* <ProjectAllCards project={projects} /> */}
+                    <Divider sx={styles.dividerStyles} />
+                  </Grid>
+                ))}
+              </Grid>
+            </Box>
+          </Box>
+        </Box>
+      )}
+
+      <Typography variant="h6" sx={{ mt: "1rem" }}>
+        Contact us for development inquiries
+      </Typography>
+      <Link href="/contactUs">
+        <Button variant="contained" size="large" sx={styles.contactButton}>
+          Learn More
         </Button>
-      </Box>
-    </Container>
+      </Link>
+    </Box>
   );
 }
 
-export default Error;
+export const getStaticProps: GetStaticProps = async ({
+  preview = false,
+  previewData,
+}) => {
+  const developerQuery = '*[_type == "development"]';
+  const client = preview ? previewClient : sanityClient;
+  const params = preview ? previewData : {};
+  const developments = await client.fetch(developerQuery, params);
+
+  return {
+    props: {
+      developments,
+      preview,
+    },
+    revalidate: 60,
+  };
+};
+
+export default AllDevelopments;
