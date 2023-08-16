891889fdc5ff6561b394bd14a79c6a7860df3d52
RabGh <rabiighais@gmail.com>
2023-08-16 16:42:34 +0300

    Payment plan styles to function

--- src/components/slugComponents/pageSlugComponents/paymentPlanSlug/paymentPlan.tsx
+++ src/components/slugComponents/pageSlugComponents/paymentPlanSlug/paymentPlan.tsx
@@ -1,10 +1,5 @@
 import { Box, Typography } from "@mui/material";
-import {
-  mainStyles,
-  cardStyles,
-  boxStyles,
-  mainTitle,
-} from "./paymentPlanStyles";
+import { getPaymentPlanStyles } from "./paymentPlanStyles";
 import { PaymentPlan } from "@lib/types";
 
 interface PaymentPlanSlugProps {
@@ -13,21 +8,22 @@ interface PaymentPlanSlugProps {
 
 const PaymentPlanSlug = ({ paymentPlan }: PaymentPlanSlugProps) => {
   const plan = paymentPlan;
+  const styles = getPaymentPlanStyles();
 
   return (
-    <Box sx={{ ...cardStyles, alignItems: "left" }}>
-      <Typography variant="h3" sx={mainTitle}>
+    <Box sx={{ ...styles.cardStyles, alignItems: "left" }}>
+      <Typography variant="h3" sx={styles.mainTitle}>
         Payment Plan
       </Typography>
       <Box
         sx={{
-          ...boxStyles,
+          ...styles.boxStyles,
           gridTemplateColumns: "repeat(2, 6fr)",
           justifyContent: "space-between",
           gridColumnGap: "3rem",
         }}
       >
-        <Box sx={mainStyles}>
+        <Box sx={styles.mainStyles}>
           {plan.paymentPlanPoints ? (
             plan.paymentPlanPoints?.map((paymentPlanPoint) => (
               <Typography key={paymentPlanPoint} variant="body1">
@@ -35,7 +31,7 @@ const PaymentPlanSlug = ({ paymentPlan }: PaymentPlanSlugProps) => {
               </Typography>
             ))
           ) : (
-            <Typography variant="body1" sx={mainStyles}>
+            <Typography variant="body1" sx={styles.mainStyles}>
               No Payment Plan Available
             </Typography>
           )}
--- src/components/slugComponents/pageSlugComponents/paymentPlanSlug/paymentPlanStyles.ts
+++ src/components/slugComponents/pageSlugComponents/paymentPlanSlug/paymentPlanStyles.ts
@@ -1,36 +1,44 @@
-export const mainContainer = {
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  justifyContent: "center",
-};
+import { useTheme } from "@mui/material";
 
-export const cardStyles = {
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "flex-start",
-  justifyContent: "flex-start",
-  mr: "2rem",
-};
+export const getPaymentPlanStyles = () => {
+  const muiTheme = useTheme();
 
-export const boxStyles = {
-  display: "grid",
-  gap: "0px",
-};
+  return {
+    mainContainer: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+    },
 
-export const mainTitle = {
-  mb: "1rem",
-  display: "flex",
-  alignItems: "flex-start",
-  justifyContent: "left",
-  [muiTheme.breakpoints.down("smallphones")]: {
-    mr: "3rem",
-  },
-};
+    cardStyles: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "flex-start",
+      justifyContent: "flex-start",
+      mr: "2rem",
+    },
 
-export const mainStyles = {
-  display: "flex",
-  flexWrap: "wrap",
-  justifyContent: "left",
-  alignItems: "flex-start",
+    boxStyles: {
+      display: "grid",
+      gap: "0px",
+    },
+
+    mainTitle: {
+      mb: "1rem",
+      display: "flex",
+      alignItems: "flex-start",
+      justifyContent: "left",
+      [muiTheme.breakpoints.down("smallphones")]: {
+        mr: "3rem",
+      },
+    },
+
+    mainStyles: {
+      display: "flex",
+      flexWrap: "wrap",
+      justifyContent: "left",
+      alignItems: "flex-start",
+    },
+  };
 };
